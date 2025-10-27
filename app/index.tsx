073f431a9f447048868e12b6b3305e846cb98547
MukeshAbhi <abhimukesh284@gmail.com>
2025-10-27 15:45:40 +0530

    routing and dynamic routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white text-2xl font-bold">Movies app</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl text-dark-200 font-bold">Well Come to Movies App</Text>
+      <Link href="/newroute">Go to new route</Link>
+      <Link href="/movie/avatar">Avatar</Link>     
     </View>
   );
 }
---
+++ app/newroute.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const newroute = () => {
+  return (
+    <View>
+      <Text>newroute</Text>
+    </View>
+  )
+}
+
+export default newroute
